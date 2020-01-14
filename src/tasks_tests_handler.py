d25912df1dfa7c213d7f9b2a7a4c5ee2c382697c
nastya <anastasia.i.birillo@gmail.com>
2020-01-14 12:46:57 +0300

    add java code support

--- src/tasks_tests_handler.py
+++ src/tasks_tests_handler.py
@@ -1,12 +1,12 @@
 from src import string_helper as sh
-from subprocess import Popen, PIPE
+from subprocess import Popen, PIPE, call
 from src import consts
 import os
 
 TASKS_TESTS_PATH = './resources/tasks_tests/'
-SOURCE_FILE_NAME = 'source.py'
+SOURCE_FILE_NAME = 'source'
 TASKS = ['pies', 'max_3', 'zero', 'election', 'brackets', 'max_digit']
-PYTHON_INPUT_FILE_NAME = 'in.py'
+INPUT_FILE_NAME = 'in'
 
 
 def __get_extension_by_language(items_dict: dict, language: str):
@@ -14,24 +14,29 @@ def __get_extension_by_language(items_dict: dict, language: str):
     print(extensions)
     pass
 
-# __get_extension_by_language(consts.LANGUAGES_DICT, 'PYTHON')
 
-
-def __create_source_code_file(source_code: str, task: str, language: str, source_file_name=SOURCE_FILE_NAME):
-    with open(TASKS_TESTS_PATH + task + '/' + source_file_name, 'w') as f:
+def __create_source_code_file(source_code: str, task: str, extension='py', source_file_name=SOURCE_FILE_NAME):
+    with open(TASKS_TESTS_PATH + task + '/' + source_file_name + '.' + extension, 'w') as f:
         f.write(source_code)
 
 
-def __drop_source_code_file(source_code_file: str, task: str):
-    os.remove(TASKS_TESTS_PATH + task + '/' + source_code_file)
+def __drop_file(file: str, task: str):
+    os.remove(TASKS_TESTS_PATH + task + '/' + file)
 
 
-def __get_out_from_out_file(out_file: str):
-    with open(TASKS_TESTS_PATH + 'pies' + '/' + out_file, 'r') as f:
-        return f.readline()
+# Get the rows from file for comparing it with a program result
+def __get_rows_from_file(out_file: str, task: str):
+    with open(TASKS_TESTS_PATH + task + '/' + out_file, 'r') as f:
+        return f.readlines()
+
+
+def __get_input(in_file: str, task: str):
+    content = __get_rows_from_file(in_file, task)
+    return ''.join(content)
 
 
 def __get_index_out_file_for_in_file(in_file: str, out_files: list):
+    # In and out files have similar numbers, for example the in_1 file match to the out_1 file
     return sh.index_containing_substring(out_files, in_file.split('.')[0].split('_')[-1])
 
 
@@ -44,9 +49,9 @@ def __separate_in_and_out_files(list_of_files: list):
     return in_files_list, out_files_list
 
 
-def __run_test(in_file: str, out: str, task: str):
+def __run_python_test(in_file: str, out: str, task: str, extension='py', source_file_name=SOURCE_FILE_NAME):
     p1 = Popen(['python', TASKS_TESTS_PATH + task + '/' + in_file], stdout=PIPE)
-    p2 = Popen(['python', TASKS_TESTS_PATH + task + '/' + SOURCE_FILE_NAME], stdin=p1.stdout, stdout=PIPE)
+    p2 = Popen(['python', TASKS_TESTS_PATH + task + '/' + source_file_name + '.' + extension], stdin=p1.stdout, stdout=PIPE)
     p1.stdout.close()
     output = p2.communicate()[0]
     if output.decode("utf-8") == out:
@@ -54,42 +59,100 @@ def __run_test(in_file: str, out: str, task: str):
     return False
 
 
-def __check_test_for_task(source_code: str, in_file: str, out_file: str, task: str, language: str):
-    __create_source_code_file(source_code, task, language)
-    res = __run_test(in_file, __get_out_from_out_file(out_file), task)
-    __drop_source_code_file(SOURCE_FILE_NAME, task)
+def __get_java_class(source_code: str):
+    class_key_word = 'class'
+    rows = source_code.split('\n')
+    for row in rows:
+        if class_key_word in row:
+            class_index = row.index(class_key_word)
+            return row[class_index + len(class_key_word) + 1:].replace(' ', '').replace('{', '')
+    raise ValueError('Source code does not contain class name!')
+
+
+def __run_java_test(in_file: str, out: str, task: str, source_file_name=SOURCE_FILE_NAME, extension='java'):
+    source_path = TASKS_TESTS_PATH + task + '/' + source_file_name + '.' + extension
+    if call(['javac', source_path]) != 0:
+        # Error
+        return False
+
+    p = Popen(['java', '-cp', TASKS_TESTS_PATH + task, source_file_name],
+              stdin=PIPE, stdout=PIPE, stderr=PIPE,
+              universal_newlines=True)
+    cur_out, err = p.communicate(input=__get_input(in_file, task))
+    if p.returncode == 0:
+        if cur_out == out:
+            return True
+    return False
+
+
+def __check_test_for_task(source_code: str, in_file: str, out_file: str, task: str, extension='py', source_file_name=SOURCE_FILE_NAME):
+    if extension == 'java':
+        source_file_name = __get_java_class(source_code)
+    __create_source_code_file(source_code, task, extension, source_file_name)
+    res = False
+    # Todo: add other languages
+    if extension == 'py':
+        res = __run_python_test(in_file, __get_rows_from_file(out_file, task)[0], task)
+    elif extension == 'java':
+        res = __run_java_test(in_file, __get_rows_from_file(out_file, task)[0], task, source_file_name)
+    __drop_file(source_file_name + '.' + extension, task)
+    if extension == 'java':
+        __drop_file(source_file_name + '.' + 'class', task)
     return res
 
 
-def __create_py_input_file(txt_in_file: str, task: str, file_name=PYTHON_INPUT_FILE_NAME):
+# Wrap all values from input in the print command
+def __create_py_input_file(txt_in_file: str, task: str, extension='py', file_name=INPUT_FILE_NAME):
     code = ''
     with open(TASKS_TESTS_PATH + task + '/' + txt_in_file, 'r') as f:
         for line in f:
             code += 'print(' + line.strip('\n') + ')' + '\n'
-    __create_source_code_file(code, task, file_name)
+    __create_source_code_file(code, task, extension, file_name)
 
 
-def check_task(task: str, source_code: str, language: str):
+# For python scripts it is an in file with extension py and for other cases, it is an in file with extension txt
+def __get_test_in_file(out_index: int, cur_in_file: str, extension='py'):
+    if extension == 'py':
+        return INPUT_FILE_NAME + '.' + extension
+    return cur_in_file
+
+
+# Clear not in\out files for correct program execution
+def __clear_old_files(task: str):
+    files = next(os.walk(TASKS_TESTS_PATH + task))[2]
+    old_files = list(filter(lambda file_name: '.txt' not in file_name, files))
+    for file in old_files:
+        __drop_file(file, task)
+
+
+def check_task(task: str, source_code: str, extension='py'):
+    __clear_old_files(task)
     files = next(os.walk(TASKS_TESTS_PATH + task))[2]
     in_files, out_files = __separate_in_and_out_files(files)
     count_tests = 0
     passed_tests = 0
 
     for cur_in in in_files:
-        __create_py_input_file(cur_in, task)
+        if extension == 'py':
+            __create_py_input_file(cur_in, task)
         out_index = __get_index_out_file_for_in_file(cur_in, out_files)
         if out_index == -1:
             continue
+        test_in = __get_test_in_file(out_index, cur_in, extension)
         count_tests += 1
-        res = __check_test_for_task(source_code, PYTHON_INPUT_FILE_NAME,
-                                    out_files[__get_index_out_file_for_in_file(cur_in, out_files)], task, language)
+        res = __check_test_for_task(source_code, test_in,
+                                    out_files[__get_index_out_file_for_in_file(cur_in, out_files)], task, extension)
         if res:
             passed_tests += 1
-    __drop_source_code_file(PYTHON_INPUT_FILE_NAME, task)
+    if extension == 'py':
+        __drop_file(INPUT_FILE_NAME + '.' + extension, task)
     return count_tests, passed_tests
 
 
-# code_1 = 'a = int(input())\nb = int(input())\nn = int(input())\nprint(str(a * n) + " " + str((b * n)))'
-# code_2 = 'a = int(input())\nb = int(input())\nc = int(input())\nprint(max(a, b, c))'
-# code_3 = 'a = int(input())\nb = int(input())\nc = int(input())\nd = int(input())\nprint(max(a, b, c))'
-# print(check_task('pies', code_3))
+code_1 = 'a = int(input())\nb = int(input())\nn = int(input())\nprint(str(a * n) + " " + str((b * n)))'
+code_2 = 'a = int(input())\nb = int(input())\nn = int(input())\nprint(str(a * n) + " " + str((b * n)))'
+code_3 = 'a = int(input())\nb = int(input())\nn = int(input())\nprint(str(a * n) + " " + str((b * n)))'
+java_code_1 = 'import java.io.*;\nclass test { \n\n    public static void main(String args[])throws IOException\n    {\n        InputStreamReader in=new InputStreamReader(System.in);\n        BufferedReader br=new BufferedReader(in);\n       int a=Integer.parseInt(br.readLine());\n        int b=Integer.parseInt(br.readLine());\n       int n=Integer.parseInt(br.readLine());\n        System.out.println(a * n + " " + b * n);\n    }\n}'
+print(check_task('pies', java_code_1, extension='java'))
+
+
