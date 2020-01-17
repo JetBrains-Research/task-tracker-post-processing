84467185dbde8243e25a82e384e6eeb41c7b507c
elena-lyulina <elena.lyulina@gmail.com>
2020-01-17 14:50:44 +0300

    Add plots with changes of code fragments size, colored according to choosen task

---
+++ src/plots/const.py
@@ -0,0 +1,26 @@
+STATUS_COLOR_SIZE_DICT = {
+    'SOLVED': ['go', 5],
+    'NOT_SOLVED': ['ro', 5],
+}
+
+STATUS_COLOR_SIZE_DEFAULT = ['bo', 1]
+
+TASK_COLOR_DICT = {
+    'pies': 'ro',
+    'max3': 'yo',
+    'election': 'go',
+    'isZero': 'co',
+    'maxDigit': 'bo',
+    'brackets': 'mo'
+}
+
+TASK_COLOR_DEFAULT = 'ko'
+
+TASK_SPLITTING_ROWS_DELTA = 10
+TASK_SPLITTING_DIFFS_DELTA = 30
+
+AT_NAME = 'ide-events'
+
+DIFF_MAX = 30
+
+ENCODING = 'ISO-8859-1'
