5c3ea7a816b6731a8518cb4c182fa034c6f5bc7c
elena-lyulina <elena.lyulina@gmail.com>
2020-02-04 23:01:14 +0300

    Add test logging

--- src/splitting/splitting.py
+++ src/splitting/splitting.py
@@ -1,11 +1,15 @@
+import logging
 import os
 from collections import defaultdict
 
 import pandas as pd
 
 # todo: move to consts (create shared const file)
+from scipy.signal import find_peaks
+
+from src.main import consts
 from src.main.code_tracker_handler import get_language
-from src.main.consts import ENCODING, ACTIVITY_TRACKER_FILE_NAME, MAX_DIFF_SYMBOLS, CODE_TRACKER_COLUMN
+from src.main.consts import ENCODING, ACTIVITY_TRACKER_FILE_NAME, MAX_DIFF_SYMBOLS, CODE_TRACKER_COLUMN, LANGUAGE
 from src.main.tasks_tests_handler import get_most_likely_tasks
 
 
@@ -13,7 +17,7 @@ def get_all_files(root: str):
     cd_files = []
     for path, subdirs, files in os.walk(root):
         for name in files:
-            if ACTIVITY_TRACKER_FILE_NAME not in name:
+            if '.csv' in name and ACTIVITY_TRACKER_FILE_NAME not in name:
                 cd_files.append(os.path.join(path, name))
     return cd_files
 
@@ -57,34 +61,63 @@ def obvious_split(data: pd.DataFrame):
 
 
 def split_with_test(data: pd.DataFrame):
-    fragment_df = data[CODE_TRACKER_COLUMN.FRAGMENT.value].astype(str)
+    fragment_df = data[CODE_TRACKER_COLUMN.FRAGMENT.value].fillna("").astype(str)
     next_fragment = fragment_df.iat[0]
     language = get_language(data)
     splits = []
     prev_split_i = 0
+    if language is not LANGUAGE.NOT_DEFINED.value:
+        for ct_i in range(data.shape[0] - 1):
+            curr_fragment = next_fragment
+            next_fragment = fragment_df.iat[ct_i + 1]
+            diff = len(curr_fragment) - len(next_fragment)
 
-    for ct_i in range(data.shape[0] - 1):
-        curr_fragment = next_fragment
-        next_fragment = fragment_df.iat[ct_i + 1]
-        diff = len(curr_fragment) - len(next_fragment)
+            if diff >= MAX_DIFF_SYMBOLS:
+                tasks_by_tests, rate = get_most_likely_tasks(curr_fragment, language)
+                if rate > 0:
+                    splits.append(ct_i)
 
-        if diff >= MAX_DIFF_SYMBOLS:
-            print("max")
-            tasks_by_tests, rate = get_most_likely_tasks(curr_fragment, language)
-            print(tasks_by_tests, rate, ct_i)
+    return splits
+
+def split_with_test_local_max(data: pd.DataFrame):
+    fragment_df = data[CODE_TRACKER_COLUMN.FRAGMENT.value].fillna("").astype(str)
+    language = get_language(data)
+    splits = []
+    prev_split_i = 0
+    if language is not LANGUAGE.NOT_DEFINED.value:
+        peaks = find_peaks(fragment_df.str.len(), distance=1)
+        for p in peaks[0]:
+            print(type(p))
+            print(p)
+            print(type(peaks))
+            print(peaks[0])
+            fragment = fragment_df.iat[p]
+            tasks_by_tests, rate = get_most_likely_tasks(fragment, language)
+            if rate > 0:
+                splits.append(p)
+    print(len(splits))
+    return splits
 
 
 def main():
+    logging.basicConfig(filename=consts.LOGGER_FILE, level=logging.INFO)
+
     path = "/home/elena/workspaces/python/codetracker-data/data/data_16_12_19"
     files = get_all_files(path)
     splits = defaultdict(list)
-    for file in files:
+
+    for i, file in enumerate(files):
+        print(file, str(i) + "/" + str(len(files)))
         data = pd.read_csv(file, encoding=ENCODING)
-        print(file)
-        split_with_test(data)
+        s = split_with_test(data)
         # s = obvious_split(data)
-        # splits[len(s)].append([file, s])
-    # print(len(splits[0]), len(files))
+        splits[len(s)].append([file, s])
+    print(splits)
+
+    # file = "/home/elena/workspaces/python/codetracker-data/data/data_16_12_19/ati_259/lol_61089_2007819470_617822553163.2119_b9e17c7947e3e679a4a5b9a1a270b8bab21426dc.csv"
+    # data = pd.read_csv(file)
+    # s = split_with_test(data)
+
 
 
 if __name__ == "__main__":
