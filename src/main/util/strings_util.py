d4537ae272d4fe7aa072d189f10842f12a24713e
elena-lyulina <elena.lyulina@gmail.com>
2020-02-18 03:25:16 +0300

    Fix quotes and bug in strings_util

--- src/main/util/strings_util.py
+++ src/main/util/strings_util.py
@@ -1,5 +1,5 @@
 def does_string_contain_any_of_substring(string: str, substrings: list, ):
-    for substring in string:
+    for substring in substrings:
         if substring in string:
             return True
     return False
