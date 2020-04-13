08afe1ae62fe6d41d1921f3b68c38e6e867b4f8f
nastya <anastasia.i.birillo@gmail.com>
2020-04-13 19:41:12 +0300

    Add todo comment

--- src/test/solution_space/path_finder/path_finder_algo_tests.py
+++ src/test/solution_space/path_finder/path_finder_algo_tests.py
@@ -11,4 +11,5 @@ log = logging.getLogger(LOGGER_NAME)
 def run_test(task: TASK, test_prefix: str) -> None:
     sg = get_solution_graph(task, test_prefix=test_prefix)
     user_solutions = get_user_solutions(task)
+    # Todo: run path finder algo and write result to the file
     pass
