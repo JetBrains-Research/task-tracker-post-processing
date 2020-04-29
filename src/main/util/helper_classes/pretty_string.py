8bfcff07ac619fd3b16b2a434a40e8c3f786b6b3
elena-lyulina <elena.lyulina@gmail.com>
2020-04-29 12:07:52 +0300

    Rename method in pretty_string

--- src/main/util/helper_classes/pretty_string.py
+++ src/main/util/helper_classes/pretty_string.py
@@ -4,13 +4,13 @@ from src.main.util.strings_util import convert_camel_case_to_snake_case
 
 
 class PrettyString:
+    _separator = '_______________'
 
-    def separator_str_for_pretty_string_method(self, suffix: str) -> str:
-        separator = '_______________'
+    def get_class_separator(self, suffix: str) -> str:
         class_name = convert_camel_case_to_snake_case(self.__class__.__name__).upper()
-        return f'\n\n{separator}{class_name}_{suffix}{separator}\n\n'
+        return f'\n\n{self._separator}{class_name}_{suffix}{self._separator}\n\n'
 
     def get_pretty_string(self) -> str:
-        return f'{self.separator_str_for_pretty_string_method("START")}' \
+        return f'{self.get_class_separator("START")}' \
                f'{self.__str__()}' \
-               f'{self.separator_str_for_pretty_string_method("END")}'
+               f'{self.get_class_separator("END")}'
