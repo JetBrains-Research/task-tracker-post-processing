51e4cb4303a70f80ae02448f20a88439009b9ede
nastya <anastasia.i.birillo@gmail.com>
2020-04-30 15:38:12 +0300

    Add try...except into graph deserialize method

--- src/main/solution_space/solution_space_serializer.py
+++ src/main/solution_space/solution_space_serializer.py
@@ -8,6 +8,7 @@ from src.main.solution_space.solution_graph import SolutionGraph
 from src.main.util.consts import SERIALIZED_GRAPH_PATH, EXTENSION, LOGGER_NAME
 from src.main.util.file_util import is_file, serialize_data_and_write_to_file, deserialize_data_from_file, \
     remove_directory
+from src.main.util.log_util import log_and_raise_error
 
 log = logging.getLogger(LOGGER_NAME)
 
@@ -29,7 +30,12 @@ class SolutionSpaceSerializer:
         if not is_file(serialized_graph_path):
             log.info(f'Path {serialized_graph_path} is incorrect')
             return None
-        deserialized_graph: SolutionGraph = deserialize_data_from_file(serialized_graph_path)
+        try:
+            deserialized_graph: SolutionGraph = deserialize_data_from_file(serialized_graph_path)
+        except OSError:
+            log_and_raise_error(f'OSError during the deserialized graph process. Serialized graph path is '
+                                f'{serialized_graph_path}', log, OSError)
+
         if to_delete_old_graph_directory:
             remove_directory(deserialized_graph.graph_directory)
         deserialized_graph.recreate_graph_files(new_path_for_graph)
