05928d93e92f01f586335c100a6caaa0e3dcdaaa
nbirillo <Anastasia.Birillo@gmail.com>
2020-05-26 11:44:54 +0300

    Code refactoring and add tests for evaluation util

--- src/main/evaluation/statistics_gathering.py
+++ src/main/evaluation/statistics_gathering.py
@@ -1,5 +1,6 @@
 # Copyright (c) 2020 Anastasiia Birillo, Elena Lyulina
 
+import re
 import logging
 from typing import Optional, Tuple, List, Callable
 
@@ -8,7 +9,7 @@ from src.main.util.consts import UTF_ENCODING
 from src.main.util.log_util import log_and_raise_error
 from src.main.util.file_util import get_content_from_file
 from src.main.evaluation.util import HINT_SIZE, HINT_STRUCTURE, HINT_TO_SOLUTION_DISTANCE, HINT_STEP, HINT_QUALITY, \
-    APPLY_DIFFS_QUALITY, HINT_SOLUTION
+    QUALITY_AFTER_DIFFS_APPLIED, HINT_SOLUTION
 
 log = logging.getLogger(consts.LOGGER_NAME)
 
@@ -17,7 +18,8 @@ class FragmentStatistics:
     def __init__(self, fragment_id: str, is_not_solution: bool, is_solution: bool,
                  hint_size: Optional[HINT_SIZE], hint_structure: Optional[HINT_STRUCTURE],
                  hint_to_solution_distance: Optional[HINT_TO_SOLUTION_DISTANCE], hint_step: Optional[HINT_STEP],
-                 hint_quality: Optional[HINT_QUALITY], apply_diffs_quality: Optional[APPLY_DIFFS_QUALITY]):
+                 hint_quality: Optional[HINT_QUALITY], apply_diffs_quality: Optional[QUALITY_AFTER_DIFFS_APPLIED]):
+        log.info(f'Start creating FragmentStatistics for the fragment with id {fragment_id}')
         self._fragment_id = fragment_id
         self._hint_solution = HINT_SOLUTION.get_hint_solution(is_solution, is_not_solution)
         self._hint_size = hint_size
@@ -26,6 +28,7 @@ class FragmentStatistics:
         self._hint_step = hint_step
         self._hint_quality = hint_quality
         self._apply_diffs_quality = apply_diffs_quality
+        log.info(f'Finish creating FragmentStatistics for the fragment with id {fragment_id}')
 
     @property
     def fragment_id(self) -> str:
@@ -56,7 +59,7 @@ class FragmentStatistics:
         return self._hint_quality
 
     @property
-    def apply_diffs_quality(self) -> Optional[APPLY_DIFFS_QUALITY]:
+    def apply_diffs_quality(self) -> Optional[QUALITY_AFTER_DIFFS_APPLIED]:
         return self._apply_diffs_quality
 
 
@@ -69,16 +72,17 @@ class EvaluationStatisticsHandler:
     def __init__(self, file: str, max_number_of_fragments: int = 100):
         self._fragments = self.__parse_file_with_statistics(file, max_number_of_fragments=max_number_of_fragments)
 
-    def __does_contain_labeled_symbol(self, items: List[str]) -> List[bool]:
-        return [self.labeled_symbol in item for item in items]
+    @classmethod
+    def __does_contain_labeled_symbol(cls, items: List[str]) -> List[bool]:
+        return [cls.labeled_symbol in item for item in items]
 
     # Return (fragment_id, is_not_solution, HINT_SIZE)
     def __handle_not_solution_piece(self, not_solution_piece: str) -> Tuple[str, bool, Optional[HINT_SIZE]]:
+        log.info(f'Start handling not solution piece of statistics')
         not_solution_piece = not_solution_piece.lstrip(self.separator).split(self.separator)
-        # Todo: add into class variables
         if len(not_solution_piece) != 4:
             log_and_raise_error(f'Not solution piece in statistics has an incorrect structure', log)
-        fragment_id = not_solution_piece[0]
+        fragment_id = not_solution_piece[0].rstrip('.')
         is_not_solution = self.labeled_symbol in not_solution_piece[1]
         return fragment_id, is_not_solution, \
                HINT_SIZE.get_hint_size(*self.__does_contain_labeled_symbol(not_solution_piece[2:]))
@@ -88,9 +92,9 @@ class EvaluationStatisticsHandler:
                                                                     Optional[HINT_TO_SOLUTION_DISTANCE],
                                                                     Optional[HINT_STEP],
                                                                     Optional[HINT_QUALITY],
-                                                                    Optional[APPLY_DIFFS_QUALITY]]:
+                                                                    Optional[QUALITY_AFTER_DIFFS_APPLIED]]:
+        log.info(f'Start handling solution piece of statistics')
         solution_piece = solution_piece.split(self.separator * 2)
-        # Todo: add into class variables
         if len(solution_piece) != 5:
             log_and_raise_error(f'Solution piece in statistics has an incorrect structure', log)
 
@@ -102,7 +106,7 @@ class EvaluationStatisticsHandler:
         converters = [HINT_TO_SOLUTION_DISTANCE.get_hint_to_solution_distance,
                       HINT_STEP.get_hint_step,
                       HINT_QUALITY.get_hint_quality,
-                      APPLY_DIFFS_QUALITY.get_apply_diffs_quality]
+                      QUALITY_AFTER_DIFFS_APPLIED.get_apply_diffs_quality]
         parsed_info = []
         for index, piece in enumerate(solution_piece[1:]):
             info = piece.split(self.separator + self.table_separator)
@@ -110,9 +114,10 @@ class EvaluationStatisticsHandler:
         return (is_solution, hint_structure, *parsed_info)
 
     def __get_statistics_from_fragment(self, fragment: str) -> FragmentStatistics:
+        log.info(f'Start parsing the fragment\n{fragment}')
         statistics = fragment.split(self.separator * 3)
         if len(statistics) != 2:
-            log_and_raise_error(f'Statistics has an incorrect structure', log)
+            log_and_raise_error(f'The fragment has an incorrect structure', log)
         fragment_id, is_not_solution, hint_size = self.__handle_not_solution_piece(statistics[0])
         is_solution, hint_structure, hint_to_solution_distance, hint_step, hint_quality, apply_diffs_quality = self.__handle_solution_piece(
             statistics[1])
@@ -128,127 +133,127 @@ class EvaluationStatisticsHandler:
         fragments = fragments[:max_number_of_fragments]
         return list(map(self.__get_statistics_from_fragment, fragments))
 
-    # Use HINT_SOLUTION: SOLUTION
-    def __get_statistics_for_solutions_by_condition(self, good_case_condition: Callable) -> Tuple[int, int]:
-        good, bad = 0, 0
-        for fragment in self._fragments:
-            if fragment.hint_solution == HINT_SOLUTION.SOLUTION:
-                if good_case_condition(fragment):
-                    good += 1
-                else:
-                    bad += 1
-        return good, bad
+    def __get_statistics_for_solutions_by_condition(self, good_case_condition: Callable,
+                                                    hint_solution_type: HINT_SOLUTION = HINT_SOLUTION.SOLUTION) -> \
+    Tuple[int, int]:
+        solutions = list(filter(lambda f: f.hint_solution == hint_solution_type, self._fragments))
+        good_solutions = list(filter(good_case_condition, solutions))
+        return len(good_solutions), len(solutions) - len(good_solutions)
 
-    # {GOOD, NORMAL} vs {BAD}
     def __get_hint_quality_statistics(self) -> Tuple[int, int]:
+        """
+            HINT_QUALITY: {GOOD, NORMAL} vs HINT_QUALITY: {BAD}
+        """
         return self.__get_statistics_for_solutions_by_condition(
             lambda f: f.hint_quality == HINT_QUALITY.GOOD or f.hint_quality == HINT_QUALITY.NORMAL)
 
-    # {CORRECT} vs {INCORRECT}
     def __get_apply_diffs_quality_statistics(self) -> Tuple[int, int]:
+        """
+            QUALITY_AFTER_DIFFS_APPLIED: {CORRECT} vs QUALITY_AFTER_DIFFS_APPLIED: {INCORRECT}
+        """
         return self.__get_statistics_for_solutions_by_condition(
-            lambda f: f.apply_diffs_quality == APPLY_DIFFS_QUALITY.CORRECT)
+            lambda f: f.apply_diffs_quality == QUALITY_AFTER_DIFFS_APPLIED.CORRECT)
 
-    # HINT_STRUCTURE: SIMILAR
-    # HINT_TO_SOLUTION_DISTANCE: APPROXIMATE
-    # HINT_STEP: NORMAL
-    # HINT_QUALITY: {GOOD, NORMAL}
-    # vs others with HINT_SOLUTION: SOLUTION
     # Todo: rename
     def __get_approximate_structure_with_good_hint_statistics(self) -> Tuple[int, int]:
+        """
+            HINT_STRUCTURE: SIMILAR
+            HINT_TO_SOLUTION_DISTANCE: APPROXIMATE
+            HINT_STEP: NORMAL
+            HINT_QUALITY: {GOOD, NORMAL}
+            vs others with HINT_SOLUTION: SOLUTION
+        """
         return self.__get_statistics_for_solutions_by_condition(lambda f:
                                                                 f.hint_structure == HINT_STRUCTURE.SIMILAR and
                                                                 f.hint_to_solution_distance == HINT_TO_SOLUTION_DISTANCE.APPROXIMATE and
                                                                 f.hint_step == HINT_STEP.NORMAL and
-                                                                (f.hint_quality == HINT_QUALITY.GOOD or f.hint_quality == HINT_QUALITY.NORMAL))
+                                                                (
+                                                                            f.hint_quality == HINT_QUALITY.GOOD or f.hint_quality == HINT_QUALITY.NORMAL))
 
-    # HINT_STRUCTURE: DISSIMILAR
-    # HINT_TO_SOLUTION_DISTANCE: APPROXIMATE
-    # HINT_STEP: NORMAL
-    # HINT_QUALITY: {GOOD, NORMAL}
-    # vs others with HINT_SOLUTION: SOLUTION
     # Todo: rename
     def __get_dissimilar_structure_with_good_hint_statistics(self) -> Tuple[int, int]:
+        """
+            HINT_STRUCTURE: DISSIMILAR
+            HINT_TO_SOLUTION_DISTANCE: APPROXIMATE
+            HINT_STEP: NORMAL
+            HINT_QUALITY: {GOOD, NORMAL}
+            vs others with HINT_SOLUTION: SOLUTION
+        """
         return self.__get_statistics_for_solutions_by_condition(lambda f:
                                                                 f.hint_structure == HINT_STRUCTURE.DISSIMILAR and
                                                                 f.hint_to_solution_distance == HINT_TO_SOLUTION_DISTANCE.APPROXIMATE and
                                                                 f.hint_step == HINT_STEP.NORMAL and
-                                                                (f.hint_quality == HINT_QUALITY.GOOD or f.hint_quality == HINT_QUALITY.NORMAL))
+                                                                (
+                                                                            f.hint_quality == HINT_QUALITY.GOOD or f.hint_quality == HINT_QUALITY.NORMAL))
 
-    # HINT_STRUCTURE: SIMILAR
-    # HINT_TO_SOLUTION_DISTANCE: APPROXIMATE
-    # HINT_STEP: {BIG, SMALL}
-    # HINT_QUALITY: {GOOD, NORMAL}
-    # vs others with HINT_SOLUTION: SOLUTION
     # Todo: rename
     def __get_approximate_structure_with_different_steps_statistics(self) -> Tuple[int, int]:
+        """
+            HINT_STRUCTURE: SIMILAR
+            HINT_TO_SOLUTION_DISTANCE: APPROXIMATE
+            HINT_STEP: {BIG, SMALL}
+            HINT_QUALITY: {GOOD, NORMAL}
+            vs others with HINT_SOLUTION: SOLUTION
+        """
         return self.__get_statistics_for_solutions_by_condition(lambda f:
                                                                 f.hint_structure == HINT_STRUCTURE.SIMILAR and
                                                                 f.hint_to_solution_distance == HINT_TO_SOLUTION_DISTANCE.APPROXIMATE and
-                                                                (f.hint_step == HINT_STEP.BIG or f.hint_step == HINT_STEP.SMALL) and
-                                                                (f.hint_quality == HINT_QUALITY.GOOD or f.hint_quality == HINT_QUALITY.NORMAL))
+                                                                (
+                                                                            f.hint_step == HINT_STEP.BIG or f.hint_step == HINT_STEP.SMALL) and
+                                                                (
+                                                                            f.hint_quality == HINT_QUALITY.GOOD or f.hint_quality == HINT_QUALITY.NORMAL))
 
-    # HINT_STRUCTURE: SIMILAR
-    # HINT_TO_SOLUTION_DISTANCE: APPROXIMATE
-    # HINT_QUALITY: {GOOD, NORMAL}
-    # vs others with HINT_SOLUTION: SOLUTION
     # Todo: rename
     def __get_approximate_structure_without_steps_statistics(self) -> Tuple[int, int]:
+        """
+            HINT_STRUCTURE: SIMILAR
+            HINT_TO_SOLUTION_DISTANCE: APPROXIMATE
+            HINT_QUALITY: {GOOD, NORMAL}
+            vs others with HINT_SOLUTION: SOLUTION
+        """
         return self.__get_statistics_for_solutions_by_condition(lambda f:
                                                                 f.hint_structure == HINT_STRUCTURE.SIMILAR and
                                                                 f.hint_to_solution_distance == HINT_TO_SOLUTION_DISTANCE.APPROXIMATE and
-                                                                (f.hint_quality == HINT_QUALITY.GOOD or f.hint_quality == HINT_QUALITY.NORMAL))
+                                                                (
+                                                                            f.hint_quality == HINT_QUALITY.GOOD or f.hint_quality == HINT_QUALITY.NORMAL))
 
-    # HINT_STRUCTURE: SIMILAR
-    # HINT_TO_SOLUTION_DISTANCE: APPROXIMATE
-    # HINT_STEP: {BIG, NORMAL}
-    # HINT_QUALITY: {GOOD, NORMAL}
-    # vs others with HINT_SOLUTION: SOLUTION
     # Todo: rename
     def __get_approximate_structure_with_different_big_and_normal_steps_statistics(self) -> Tuple[int, int]:
+        """
+            HINT_STRUCTURE: SIMILAR
+            HINT_TO_SOLUTION_DISTANCE: APPROXIMATE
+            HINT_STEP: {BIG, NORMAL}
+            HINT_QUALITY: {GOOD, NORMAL}
+            vs others with HINT_SOLUTION: SOLUTION
+        """
         return self.__get_statistics_for_solutions_by_condition(lambda f:
                                                                 f.hint_structure == HINT_STRUCTURE.SIMILAR and
                                                                 f.hint_to_solution_distance == HINT_TO_SOLUTION_DISTANCE.APPROXIMATE and
-                                                                (f.hint_step == HINT_STEP.BIG or f.hint_step == HINT_STEP.NORMAL) and
-                                                                (f.hint_quality == HINT_QUALITY.GOOD or f.hint_quality == HINT_QUALITY.NORMAL))
+                                                                (
+                                                                            f.hint_step == HINT_STEP.BIG or f.hint_step == HINT_STEP.NORMAL) and
+                                                                (
+                                                                            f.hint_quality == HINT_QUALITY.GOOD or f.hint_quality == HINT_QUALITY.NORMAL))
 
     @staticmethod
-    def __get_percents(current: int, all: int) -> float:
+    def __get_readable_percents(current: int, all: int) -> float:
         return round((current / all) * 100, 2)
 
-    def __print_one_statistics(self, good: int, bad: int, comment: str) -> None:
-        all = good + bad
-        print(f'{comment}\n({good} examples, {bad} examples)\n'
-              f'({self.__get_percents(good, all)}%, {self.__get_percents(bad, all)}%)')
-        print(self.statistics_printer_separator)
+    @staticmethod
+    def __get_readable_title(title: str) -> str:
+        return re.sub(' +', ' ', title.strip('\n'))
 
     def print_statistics(self) -> None:
-        good, bad = self.__get_hint_quality_statistics()
-        self.__print_one_statistics(good, bad, 'Good or normal VS bad quality of a hint:')
+        methods_for_print = [self.__get_hint_quality_statistics,
+                             self.__get_apply_diffs_quality_statistics,
+                             self.__get_approximate_structure_with_good_hint_statistics,
+                             self.__get_dissimilar_structure_with_good_hint_statistics,
+                             self.__get_approximate_structure_with_different_steps_statistics,
+                             self.__get_approximate_structure_without_steps_statistics,
+                             self.__get_approximate_structure_with_different_big_and_normal_steps_statistics]
 
-        good, bad = self.__get_apply_diffs_quality_statistics()
-        self.__print_one_statistics(good, bad, 'Correct VS incorrect diffs applying:')
-
-        good, bad = self.__get_approximate_structure_with_good_hint_statistics()
-        self.__print_one_statistics(good, bad, 'Hint structure: SIMILAR\nHint to solution distance: APPROXIMATE\n'
-                                               'Hint step: NORMAL\nHint quality: {GOOD, NORMAL} VS others, which '
-                                               'are the task solution')
-
-        good, bad = self.__get_dissimilar_structure_with_good_hint_statistics()
-        self.__print_one_statistics(good, bad, 'Hint structure: DISSIMILAR\nHint to solution distance: APPROXIMATE\n'
-                                               'Hint step: NORMAL\nHint quality: {GOOD, NORMAL} VS others, which '
-                                               'are the task solution')
-
-        good, bad = self.__get_approximate_structure_with_different_steps_statistics()
-        self.__print_one_statistics(good, bad, 'Hint structure: SIMILAR\nHint to solution distance: APPROXIMATE\n'
-                                               'Hint step: {BIG, SMALL}\nHint quality: {GOOD, NORMAL} VS others, which '
-                                               'are the task solution')
-
-        good, bad = self.__get_approximate_structure_without_steps_statistics()
-        self.__print_one_statistics(good, bad, 'Hint structure: SIMILAR\nHint to solution distance: APPROXIMATE\n'
-                                               'Hint quality: {GOOD, NORMAL} VS others, which are the task solution')
-
-        good, bad = self.__get_approximate_structure_with_different_big_and_normal_steps_statistics()
-        self.__print_one_statistics(good, bad, 'Hint structure: SIMILAR\nHint to solution distance: APPROXIMATE\n'
-                                               'Hint step: {BIG, NORMAL}\nHint quality: {GOOD, NORMAL} '
-                                               'VS others, which are the task solution')
+        for m in methods_for_print:
+            good, bad = m()
+            count = good + bad
+            print(f'{self.__get_readable_title(m.__doc__)}\n({good} examples, {bad} examples)\n'
+                  f'({self.__get_readable_percents(good, count)}%, {self.__get_readable_percents(bad, count)}%)')
+            print(self.statistics_printer_separator)
--- src/main/evaluation/util.py
+++ src/main/evaluation/util.py
@@ -3,6 +3,7 @@
 import logging
 from enum import Enum
 from typing import Optional
+from itertools import repeat
 
 from src.main.util import consts
 from src.main.util.log_util import log_and_raise_error
@@ -10,43 +11,39 @@ from src.main.util.log_util import log_and_raise_error
 log = logging.getLogger(consts.LOGGER_NAME)
 
 
-#Todo: fix names int the file and add tests
-
-def at_least_two(*values) -> bool:
-    if len(values) <= 1:
-        return False
-    res = values[0]
-    for value in values[1:]:
-        res = res and value
-        if res:
-            return True
-        res = res or value
-    return False
+# Check if al least N (min_number_of_true) values are True
+def contains_at_least_n_true(*values: bool, min_number_of_true: int = 2) -> bool:
+    return all(map(any, repeat(iter(list(values)), min_number_of_true)))
 
 
+# Shows if the fragment is a solution problem or not
 class HINT_SOLUTION(Enum):
     SOLUTION = 'solution'
     NOT_SOLUTION = 'not solution'
 
     @staticmethod
     def get_hint_solution(is_solution: bool, is_not_solution: bool) -> 'HINT_SOLUTION':
-        if at_least_two(is_solution, is_not_solution):
+        if contains_at_least_n_true(is_solution, is_not_solution):
             log_and_raise_error(f'Incorrect value for the hint solution. At least two values are True', log)
         if is_solution:
             return HINT_SOLUTION.SOLUTION
         if is_not_solution:
             return HINT_SOLUTION.NOT_SOLUTION
-        log_and_raise_error(f'Undefined hint solution value', log)
+        log_and_raise_error(f'Undefined hint solution value. Passed values:\nis_solution {is_solution}\n'
+                            f'is_not_solution {is_not_solution}', log)
 
 
+# Shows a hint size
+# Note: is valid only for HINT_SOLUTION.NOT_SOLUTION otherwise is None
 class HINT_SIZE(Enum):
     SMALL = 'small'
     BIG = 'big'
 
     @staticmethod
     def get_hint_size(is_small: bool, is_big: bool) -> Optional['HINT_SIZE']:
-        if at_least_two(is_small, is_big):
-            log_and_raise_error(f'Incorrect value for the hint size. At least two values are True', log)
+        if contains_at_least_n_true(is_small, is_big):
+            log_and_raise_error(f'Incorrect value for the hint size. At least two values are True. Passed values:\n'
+                                f'is_small {is_small}\nis_big {is_big}', log)
         if is_small:
             return HINT_SIZE.SMALL
         if is_big:
@@ -54,14 +51,17 @@ class HINT_SIZE(Enum):
         return None
 
 
+# Shows a hint structure
+# Note: is valid only for HINT_SOLUTION.SOLUTION otherwise is None
 class HINT_STRUCTURE(Enum):
     SIMILAR = 'similar'
     DISSIMILAR = 'disimilar'
 
     @staticmethod
     def get_hint_structure(is_similar: bool, is_dissimilar: bool) -> Optional['HINT_STRUCTURE']:
-        if at_least_two(is_similar, is_dissimilar):
-            log_and_raise_error(f'Incorrect value for the hint structure. At least two values are True', log)
+        if contains_at_least_n_true(is_similar, is_dissimilar):
+            log_and_raise_error(f'Incorrect value for the hint structure. At least two values are True. '
+                                f'Passed values:\nis_similar{is_similar}\nis_dissimilar {is_dissimilar}', log)
         if is_similar:
             return HINT_STRUCTURE.SIMILAR
         if is_dissimilar:
@@ -69,6 +69,8 @@ class HINT_STRUCTURE(Enum):
         return None
 
 
+# Shows a distance between hint and current solution structure
+# Note: is valid only for HINT_SOLUTION.SOLUTION otherwise is None
 class HINT_TO_SOLUTION_DISTANCE(Enum):
     APPROXIMATE = 'approximate'
     SAME_TREE = 'same tree'
@@ -78,8 +80,10 @@ class HINT_TO_SOLUTION_DISTANCE(Enum):
     @staticmethod
     def get_hint_to_solution_distance(is_approximate: bool, is_same_tree: bool, is_remote: bool,
                                       is_unclear: bool) -> Optional['HINT_TO_SOLUTION_DISTANCE']:
-        if at_least_two(is_approximate, is_same_tree, is_remote, is_unclear):
-            log_and_raise_error(f'Incorrect value for the hint to solution distance. At least two values are True', log)
+        if contains_at_least_n_true(is_approximate, is_same_tree, is_remote, is_unclear):
+            log_and_raise_error(f'Incorrect value for the hint to solution distance. At least two values are True. '
+                                f'Passed values:\nis_approximate {is_approximate}\nis_same_tree {is_same_tree}\n'
+                                f'is_remote {is_remote}\nis_unclear {is_unclear}', log)
         if is_approximate:
             return HINT_TO_SOLUTION_DISTANCE.APPROXIMATE
         if is_same_tree:
@@ -91,6 +95,8 @@ class HINT_TO_SOLUTION_DISTANCE(Enum):
         return None
 
 
+# Shows a hint step size
+# Note: is valid only for HINT_SOLUTION.SOLUTION otherwise is None
 class HINT_STEP(Enum):
     NORMAL = 'normal'
     BIG = 'big'
@@ -98,8 +104,9 @@ class HINT_STEP(Enum):
 
     @staticmethod
     def get_hint_step(is_normal: bool, is_big: bool, is_small: bool) -> Optional['HINT_STEP']:
-        if at_least_two(is_normal, is_big, is_small):
-            log_and_raise_error(f'Incorrect value for the hint step. At least two values are True', log)
+        if contains_at_least_n_true(is_normal, is_big, is_small):
+            log_and_raise_error(f'Incorrect value for the hint step. At least two values are True. Passed values:\n'
+                                f'is_normal {is_normal}\nis_big {is_big}\nis_small {is_small}', log)
         if is_normal:
             return HINT_STEP.NORMAL
         if is_big:
@@ -109,6 +116,8 @@ class HINT_STEP(Enum):
         return None
 
 
+# Shows a hint quality
+# Note: is valid only for HINT_SOLUTION.SOLUTION otherwise is None
 class HINT_QUALITY(Enum):
     GOOD = 'good'
     NORMAL = 'normal'
@@ -116,8 +125,9 @@ class HINT_QUALITY(Enum):
 
     @staticmethod
     def get_hint_quality(is_good: bool, is_normal: bool, is_bad: bool) -> Optional['HINT_QUALITY']:
-        if at_least_two(is_good, is_normal, is_bad):
-            log_and_raise_error(f'Incorrect value for the hint quality. At least two values are True', log)
+        if contains_at_least_n_true(is_good, is_normal, is_bad):
+            log_and_raise_error(f'Incorrect value for the hint quality. At least two values are True. Passed values:\n'
+                                f'is_good {is_good}\nis_normal {is_normal}\nis_bad {is_bad}', log)
         if is_good:
             return HINT_QUALITY.GOOD
         if is_normal:
@@ -127,17 +137,19 @@ class HINT_QUALITY(Enum):
         return None
 
 
-class APPLY_DIFFS_QUALITY(Enum):
+# Shows a code quality after diffs applied
+# Note: is valid only for HINT_SOLUTION.SOLUTION otherwise is None
+class QUALITY_AFTER_DIFFS_APPLIED(Enum):
     CORRECT = 'correct'
     INCORRECT = 'incorrect'
 
     @staticmethod
-    def get_apply_diffs_quality(is_correct: bool, is_incorrect: bool) -> Optional['APPLY_DIFFS_QUALITY']:
-        if at_least_two(is_correct, is_incorrect):
-            log_and_raise_error(f'Incorrect value for the apply diffs quality. At least two values are True', log)
+    def get_apply_diffs_quality(is_correct: bool, is_incorrect: bool) -> Optional['QUALITY_AFTER_DIFFS_APPLIED']:
+        if contains_at_least_n_true(is_correct, is_incorrect):
+            log_and_raise_error(f'Incorrect value for the apply diffs quality. At least two values are True. '
+                                f'Passed values:\nis_correct {is_correct}\nis_incorrect {is_incorrect}', log)
         if is_correct:
-            return APPLY_DIFFS_QUALITY.CORRECT
+            return QUALITY_AFTER_DIFFS_APPLIED.CORRECT
         if is_incorrect:
-            return APPLY_DIFFS_QUALITY.INCORRECT
+            return QUALITY_AFTER_DIFFS_APPLIED.INCORRECT
         return None
-
